50ef74a0bcf17521bd8f816dac531e07e02cd80e
NewLight <256263487@qq.com>
2023-12-05 10:36:30 +0800

    测试更新

--- Data_label.py
+++ Data_label.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-
+#test
 plt.rcParams['font.family'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
