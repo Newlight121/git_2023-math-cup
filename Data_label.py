fc1dbed58ca779d0b2d90bc39584a64905d5876c
NewLight <256263487@qq.com>
2023-12-05 10:50:56 +0800

    Revert "测试更新"
    This reverts commit 50ef74a0

--- Data_label.py
+++ Data_label.py
@@ -4,8 +4,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-#test
-#dev_test
+
 plt.rcParams['font.family'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
